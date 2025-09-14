ce5b7b0e11263ca59005fa0a7a018752ca6103ce
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2025-09-14 15:47:03 +0200

    REmoving

--- src/Util/Response.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Src\Util;
-class Response {
-public static function json($data,int $code=200){
-http_response_code($code);
-header('Content-Type: application/json');
-echo json_encode($data);
-}
-}
